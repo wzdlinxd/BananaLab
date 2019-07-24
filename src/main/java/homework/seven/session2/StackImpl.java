dcc05833229d66d92a4e6e118eec29062e990c4b
Seven <seven@diqigan.cn>
2019-07-25 00:30:19 +0800

    #feature finish session2

--- src/main/java/homework/seven/session2/StackImpl.java
+++ src/main/java/homework/seven/session2/StackImpl.java
@@ -8,100 +8,71 @@ package homework.seven.session2;
  */
 public class StackImpl implements Stack {
   /**
+   * 栈大小
+   */
+  private int size = 0;
+
+  /**
    * 栈内元素
    */
-  private int[] elements = new int[0];
+  private int[] elements = new int[16];
 
   /**
-   * 对整型数组进行截取操作, 截取区间前闭后开
+   * 数组扩容
    *
-   * @param array 截取前的数组
-   * @param start 截取起始位置, 包含起始位置对应元素
-   * @param end   截取终止位置, 不包含终止位置对应元素
-   * @return 截取后的新数组
+   * @param array 原数组
+   * @param increaseSize 需要另外扩充的大小
+   * @return 扩容后的数组
    */
-  private int[] sliceArray(int[] array, int start, int end) {
-    if (null == array) {
-      return new int[0];
+  private int[] increaseArraySpace(int[] array, int increaseSize) {
+    if (increaseSize < 0) {
+      throw new IllegalArgumentException("increaseSize shouldn't less than 0");
+    } else if (increaseSize == 0) {
+      return array;
+    } else {
+      int[] newArray = new int[array.length + increaseSize];
+      System.arraycopy(array, 0, newArray, 0, array.length);
+      return newArray;
     }
-
-    if (start < 0 || end > array.length || start > end) {
-      throw new IndexOutOfBoundsException();
-    }
-
-    int[] result = new int[end - start];
-    for (int i = start, j = 0; i < end; i++, j++) {
-      result[j] = array[i];
-    }
-
-    return result;
-  }
-
-  /**
-   * 在转入 array 的 index 位置之前插入 element
-   * @param array 插入元素之前的数组
-   * @param element 待插入数组的元素
-   * @param index 元素插入的位置
-   * @return 在指定位置插入元素之后的新数组
-   */
-  private int[] insert2Array(int[] array, int element, int index) {
-    if (null == array) {
-      return new int[0];
-    }
-
-    if (index > array.length) {
-      throw new IndexOutOfBoundsException();
-    }
-
-    int[] result = new int[array.length + 1];
-    int currentElement;
-    for (int i = 0, j = 0; i < result.length; i++, j++) {
-      if (i != index) {
-        currentElement = array[j];
-      } else {
-        currentElement = element;
-        j--;
-      }
-
-      result[i] = currentElement;
-    }
-
-    return result;
   }
 
   @Override
   public boolean push(int element) {
-    this.elements = this.insert2Array(this.elements, element, this.elements.length);
+    size++;
+    // 自动扩容
+    if (this.size > this.elements.length) {
+      this.elements = this.increaseArraySpace(this.elements, this.elements.length);
+    }
+
+    this.elements[size - 1] = element;
     return true;
   }
 
   @Override
   public int pop() {
-    if (this.elements.length == 0) {
+    if (this.size() == 0) {
       throw new RuntimeException("this stack is empty!");
     }
 
-    int result = this.elements[this.elements.length - 1];
-    this.elements = sliceArray(this.elements, 0, this.elements.length - 1);
-    return result;
+    return this.elements[--this.size];
   }
 
   @Override
   public int peak() {
-    if (this.elements.length == 0) {
+    if (this.size() == 0) {
       throw new RuntimeException("this stack is empty!");
     }
     
-    return this.elements[this.elements.length - 1];
+    return this.elements[this.size - 1];
   }
 
   @Override
   public int size() {
-    return this.elements.length;
+    return this.size;
   }
 
   @Override
   public boolean isEmpty() {
-    return this.size() != 0;
+    return this.size() == 0;
   }
 }
