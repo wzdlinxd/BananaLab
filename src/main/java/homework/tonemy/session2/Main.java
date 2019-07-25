c68840ab7b26e29be7e76e4d271b7eca1c892e52
tonemy <1006045923@qq.com>
2019-07-25 17:51:13 +0800

    "编码问题"

--- src/main/java/homework/tonemy/session2/Main.java
+++ src/main/java/homework/tonemy/session2/Main.java
@@ -23,7 +23,6 @@ package homework.tonemy.session2;
  *
   */
 public class Main {
-
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
 		Stack stack1 = new StackImpl();
