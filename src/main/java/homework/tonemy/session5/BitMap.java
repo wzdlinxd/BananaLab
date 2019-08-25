d946931de752eccd9a70bd4e3140a206d3d235db
tonemy <1006045923@qq.com>
2019-08-25 09:41:45 +0800

    "Session 5"

--- src/main/java/homework/tonemy/session5/BitMap.java
+++ src/main/java/homework/tonemy/session5/BitMap.java
@@ -1,16 +1,18 @@
 package homework.tonemy.session5;
 
+
+
 /**
  * 去重统计的 BitMap
  * @param <K>
  */
-public class BananaBitMap<K> implements CountingInterface<K> {
+public class BitMap<K> implements CountingInterface<K> {
 
 	//每个 byte的每一位bit可以用来表示 一个数字的有无,n 表示数字中最大的一个,比如10亿，
 	//除以 8 再加一就是 bits数组的长度.
 	byte[] bits;
 	int count = 0;
-	public BananaBitMap(int n) {
+	public BitMap(int n) {
 		bits = new byte[n >> 3  + 1];
 	}
 
@@ -20,10 +22,14 @@ public class BananaBitMap<K> implements CountingInterface<K> {
 	 */
 	@Override
 	public void add(K key) {
+		if (key == null) return;
+		if(!(key instanceof Integer)) {
+			return;
+		}
 		// 找到key在bits中的索引 这里假设key为整数
-		int index =(int)key >> 8;
+		int index =(Integer) key >> 8;
 		// 找到key在bits[index]的位置
-		int pos = (int)key & 0x07;
+		int pos = (Integer) key & 0x07;
 		// 检测
 		if(!((bits[index] & 1 << pos) != 0)){
 			this.count ++;
